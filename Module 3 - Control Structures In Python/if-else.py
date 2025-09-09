53a2c9644d20524309dfff631f17d633dd9b160f
gb2378831 <gb2378831@gmail.com>
2025-09-09 05:30:33 +0530

    Change

--- Module 3 - Control Structures In Python/if-else.py
+++ Module 3 - Control Structures In Python/if-else.py
@@ -1,2 +1,14 @@
-marks = int(input('Enter your marks: '))
+# marks = int(input('Enter your marks: '))
 
+# if (marks >= 40):
+#     print("Passed")
+# else:
+#     print("Failed")
+# print("Thankyou")
+
+age = int(input("Enter your age: "))
+
+if age>18:
+    print("You can vote")
+else:
+    print("You can't vote")
