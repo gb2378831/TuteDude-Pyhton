08fc41962a8b79d4638ce5b066d82931cd710c7d
gb2378831 <gb2378831@gmail.com>
2025-09-09 05:53:52 +0530

    for-loop

---
+++ Module 3 - Control Structures In Python/for_loop.py
@@ -0,0 +1,14 @@
+# a ---> 10 times
+
+
+# l = ['Mike',10,20,1300]
+# for i in l:
+#     print(i) 
+
+# x = 'apple'
+# for i in x:
+#     print(i)
+
+for i in range(1,11):
+    # print(i)
+    print(i,"apple")
