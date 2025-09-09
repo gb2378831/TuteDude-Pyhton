f001d4384da7344497114e60a7fdd510f6e24ec3
gb2378831 <gb2378831@gmail.com>
2025-09-09 06:08:14 +0530

    while-loop

---
+++ Module 3 - Control Structures In Python/while_loop.py
@@ -0,0 +1,21 @@
+# a ---> 5 times
+
+# counter = 1
+# while counter <= 5:
+#     print(counter,"a")
+#     counter += 1
+
+# counter = 5
+# while counter >= 1:
+#     print(counter,"a")
+#     counter -= 1
+
+# counter = 0
+# while counter <=10:
+#     print(counter,"a")
+#     counter += 1
+
+counter = 1
+while counter <= 5:
+    print(counter,"Python")
+    counter += 1
