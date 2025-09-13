56270c061e64b2d20bbdb6d23ca73445c2b05628
gb2378831 <gb2378831@gmail.com>
2025-09-14 01:02:11 +0530

    errors

---
+++ Files, Exceptions and Errors/Error-Exceptions.py
@@ -0,0 +1,46 @@
+'''
+# 1--> syntax error
+
+a = 2
+if a<1:
+    print("hello")
+else <--- Here is syntax error because of missing of colon (:)
+    print("welcome")
+'''
+
+'''
+# 2--> indentation error
+
+a = 2
+if a<1:
+    print("hello")
+else:
+print("welcome") <--- Here is indentation error because of missing of indenting
+'''
+
+'''
+# 3 --> zero division error
+
+print(8/0)
+'''
+
+'''
+# 4 --> Module Error
+import mathematics
+print(mathematics.pi)
+'''
+
+'''
+# 5 --> Type Error
+
+a = 2
+b = '4'
+print(a+b)
+'''
+
+'''
+# 6 --> Logic Error
+a = 2
+b =3
+print(a + b)
+'''
