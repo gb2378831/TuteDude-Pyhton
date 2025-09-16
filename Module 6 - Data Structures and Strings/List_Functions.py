40cd84254fbb86c41c7d3fa68677d6b4566ce10e
gb2378831 <gb2378831@gmail.com>
2025-09-17 04:38:25 +0530

    lsit completed

---
+++ Module 6 - Data Structures and Strings/List_Functions.py
@@ -0,0 +1,39 @@
+# l = ['mike',10.1,1980,10,12,10]
+l = [10.1,1980,10,12,10]
+
+
+#add single value at end
+# l.append(10)
+
+#add multiple values at end
+# l.extend([10,1])
+
+#remove value
+# l.remove(10)
+
+
+#deletes in sequence
+# del l[0:3]    #delete elements from index value 0 to index value 3-1
+
+#clear list
+# l.clear()
+
+#to delete the element by index value, but it only delete one element
+# l.pop(2)
+
+#inerting elements in between of elements
+# l.insert(2,'pop')
+
+#arrange the elements of a list in a specific order
+# l.sort()
+
+#to reverse the list
+# l.reverse()
+
+#to find the index value of element
+# print(l.index(1980))
+
+#to find the length of list
+# print(len(l))
+
+print(l)
