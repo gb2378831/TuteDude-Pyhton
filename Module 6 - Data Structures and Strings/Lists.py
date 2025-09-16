f75591b984befc8b4ce4473953ce1111b43a7b31
gb2378831 <gb2378831@gmail.com>
2025-09-16 20:57:23 +0530

    list

---
+++ Module 6 - Data Structures and Strings/Lists.py
@@ -0,0 +1,30 @@
+# LIST
+# EG: Writting a list for the groceries
+'''
+a = 1
+b = 2 
+c = 3
+d = 4
+e = 5
+
+print(c)
+'''
+'''
+number = [1,2,3,4,5]
+#Index -  0 1 2 3 4
+
+print(number[2])
+'''
+
+'''
+colors = ['black', 'white','grey', 'blue', 'pink', 'orange']
+#Index -     0        1       2       3       4       5
+
+print(colors[1])
+'''
+
+mix  =  ['Peter',10,'Sam',15]
+#Index -    0     1   2    3  
+print(mix[1])
+
+blank_list = []
