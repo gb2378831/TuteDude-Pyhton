76354dc0d838d25490293f6b54ebdf4104115d35
gb2378831 <gb2378831@gmail.com>
2025-09-16 21:04:24 +0530

    list operations

---
+++ Module 6 - Data Structures and Strings/List_Operations.py
@@ -0,0 +1,20 @@
+'''
+list1 = ['Mike',10.1,1980]
+list2 = ['pop',1]
+print(list1 + list2)
+'''
+
+'''
+#Lists are MUTABLE
+a = ['disco',10,1.2]
+a[0] = 'hardwork'
+print(a)
+'''
+
+'''
+n = [1,2,3]
+print(n * 3)
+'''
+
+games = ['football','basketball','chess']
+print("football" in games)
